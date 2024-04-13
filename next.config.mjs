dde16b2511e3331d93a460cd6a16deec56354abc
blackrose54 <144953375+blackrose54@users.noreply.github.com>
2024-04-13 09:48:09 +0530

    reviewd

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'i.kickstarter.com',
-          },
-        ],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.kickstarter.com",
       },
+    ],
+  },
 };
 
 export default nextConfig;
